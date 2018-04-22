a7481c635100208bf1ac0164e1a7059de88c9b10
Seth Silesky <seth.silesky@gmail.com>
2018-04-22 15:26:09 -0500

    refactor

--- src/3-Branch.js
+++ src/3-Branch.js
@@ -8,30 +8,28 @@ import {
 } from 'recompose';
 import { withCounterState, fetchDataFromApi } from './utils';
 
+const Counter = ({ counter, add }) => (
+  <div>
+    <h1>counter: {counter}</h1>
+    <button onClick={add}>+</button>
+  </div>
+);
 
-const Counter = ({ counter, add }) => {
-  return (
-    <div>
-      <h1>counter: {counter}</h1>
-      <button onClick={add}>+</button>
-    </div>
-  );
-};
-
-// use compose reverse + lifecycle
-const withApiCall = compose(
+const withLoadData = compose(
   withStateHandlers(({ data = [] }) => ({ data }), {
     loadData: () => data => ({ data }),
   }),
+  // fetch data
   lifecycle({
     componentDidMount() {
       fetchDataFromApi().then(data => this.props.loadData(data));
     },
   }),
-);
-const withLoadingOrNot = branch(
-  props => !props.data.length, // if no data, render
-  renderComponent(() => <h2>Loading....</h2>),
+  // if data still loading, render 'loading' message
+  branch(
+    props => !props.data.length,
+    renderComponent(() => <h2>Loading....</h2>),
+  ),
 );
 
-export default compose(withCounterState, withApiCall, withLoadingOrNot)(Counter);
+export default compose(withCounterState, withLoadData)(Counter);
